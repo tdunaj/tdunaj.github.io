6f40ddc05cc79fb28a8b31adf864e7ace54dfad7
tomas.dunaj <tomas.dunaj@agilethought.com>
2018-02-09 15:23:23 -0500

    Init Commit

---
+++ SaveButtonWeb/Spec/Functions/FunctionFileSpec.ts
@@ -0,0 +1,6 @@
+﻿//empty for now the global Office functions prevent the creation of unit tests
+describe("A suite", function () {
+    it("contains spec with an expectation", function () {
+        expect(true).toBe(true);
+    });
+});
---
+++ SaveButtonWeb/Spec/test.js
@@ -0,0 +1,5 @@
+﻿import Jasmine from 'jasmine';
+
+let jasmine = new Jasmine();
+jasmine.loadConfigFile('Spec/support/jasmine.json');
+jasmine.execute();
